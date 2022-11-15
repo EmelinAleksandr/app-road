280e103f07728d40dbe611d162b4b25af75eca06
84991 <84991@PC3003>
2022-11-15 22:49:57 +0400

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace app_road
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Start = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.End = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cost = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(9, 87);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(206, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.load_end);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 426);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Вокзал";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Тип поездки";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(91, 17);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(65, 17);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Прямая";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(91, 40);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(103, 17);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "С пересадками";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(469, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.calculate);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Start,
+            this.End,
+            this.Cost});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Location = new System.Drawing.Point(9, 138);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.Size = new System.Drawing.Size(743, 282);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Start
+            // 
+            this.Start.HeaderText = "Пункт отправления";
+            this.Start.Name = "Start";
+            this.Start.ReadOnly = true;
+            this.Start.Width = 300;
+            // 
+            // End
+            // 
+            this.End.HeaderText = "Пункт прибытия";
+            this.End.Name = "End";
+            this.End.ReadOnly = true;
+            this.End.Width = 300;
+            // 
+            // Cost
+            // 
+            this.Cost.HeaderText = "Стоимость";
+            this.Cost.Name = "Cost";
+            this.Cost.ReadOnly = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Доступные билеты";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(240, 87);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(206, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(237, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Пункт прибытия";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Пункт отправления";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(631, 441);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Итого";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(675, 439);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(89, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 468);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.Text = "Межгородские поездки";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Start;
+        private System.Windows.Forms.DataGridViewTextBoxColumn End;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cost;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace app_road
+{
+    public partial class Form1 : Form
+    {
+        string[] cities = { "Город1", "Город2", "Город3", "Город4", "Город5", "Город6", "Город7", "Город8", "Город9", "Город10", "Город11", "Город12", "Город13", "Город14", "Город15", "Город16", "Город17", "Город18", "Город19" };
+        DataTable _data = new DataTable(); // данные из таблицы excel
+        int type_road = 1; // 0 - прямые перееды, 1 - с пересадками
+        int type_calc = 0; // 0 - занимающие меньшее кол-во пересадок, 1 - самы бюджетный вариант 
+
+        public Form1()
+        {
+            InitializeComponent();
+            load_data();
+
+            load_start();
+            load_end();
+            radioButton2.Select();
+        }
+
+        /// <summary>
+        /// Загрузка списка городов в первый селектор(Пункт отправления)
+        /// </summary>
+        private void load_start()
+        {
+            comboBox1.Items.AddRange(cities);
+        }
+
+        /// <summary>
+        /// Загрузка списка городов во второй селектор(Пункт прибытия)
+        /// </summary>
+        private void load_end()
+        {
+            comboBox2.ResetText();
+            if(type_road == 0)
+                comboBox2.Items.AddRange(get_cities());
+            else
+                comboBox2.Items.AddRange(cities);
+        }
+
+        /// <summary>
+        /// Сбор данных и добавление в таблицу (Доступные билеты)
+        /// </summary>
+        /// <param name="sender">Стандартный параметр, создается по умолчанию</param>
+        /// <param name="e">Стандартный параметр, создается по умолчанию</param>
+        private void calculate(object sender, EventArgs e)
+        {
+
+            dataGridView1.Rows.Clear();
+            string start = (string)comboBox1.SelectedItem;
+            string end = (string)comboBox2.SelectedItem;
+            
+            var index_r = comboBox1.SelectedIndex;
+            var index_c = cities.ToList().IndexOf(end) + 1;
+
+            var i = _data.Rows[index_r][index_c];
+            if(i != DBNull.Value)
+            {
+                dataGridView1.Rows.Add(start, end, i);
+                result(i);
+            }
+            calc(index_r, index_c);
+        }
+
+        /// <summary>
+        /// Запись данных в Итого
+        /// </summary>
+        /// <param name="result"></param>
+        private void result(object result)
+        {
+            textBox2.Text = result.ToString();
+        }
+
+        /// <summary>
+        /// ЗАгрузка таблицы Excel, для этого создается подключение к excel, котрое выступает в качестве БД
+        /// </summary>
+        private void load_data()
+        {
+            String name = "Items";
+            String constr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" +
+                            "D:\\Users\\84991\\Documents\\Sample.xlsx" + // Здесь указан путь до excel, нужно указать путь к файлу, где он у тебя храниться на ПК, по файлу ПКМ -> Свйоства и там есть пункт "Расположение", только слэши нужно указать как здесь
+                            ";Extended Properties='Excel 12.0 XML;HDR=YES;';";
+
+            OleDbConnection con = new OleDbConnection(constr);
+            OleDbCommand oconn = new OleDbCommand("Select * From [" + name + "$]", con);
+            con.Open();
+
+            OleDbDataAdapter sda = new OleDbDataAdapter(oconn);
+            sda.Fill(_data);
+        }
+
+        /// <summary>
+        /// Получение списка городов во второй селектор(Пункт прибытия) в случае, если выбрантип поездки "Прямая"
+        /// </summary>
+        /// <returns></returns>
+        private string[] get_cities()
+        {
+            comboBox2.Items.Clear();
+            var _cities = new List<string>();
+
+            if (comboBox1.SelectedItem != null)
+            {
+                var ind = comboBox1.SelectedIndex;
+               
+                for(int i = 1; i < _data.Columns.Count; i++)
+                {
+                    var n = _data.Rows[ind][i];
+                    if(n != DBNull.Value)
+                    {
+                        _cities.Add(_data.Columns[i].ToString());
+                    }
+                }
+            }
+
+            return _cities.ToArray();
+        }
+
+        /// <summary>
+        /// Это в работе
+        /// </summary>
+        /// <param name="start">Откуда начинаются все поездки</param>
+        /// <param name="end"> куда по итогу нужно приехать</param>
+        private void calc(int start, int end)
+        {
+            get_path(start, end, new List<int>());
+        }
+
+        /// <summary>
+        /// Маршруты, по идее суда должны записываться маршруты следования, из них нужно будет выбрать самы короткий и который позваоляет приехать в нужный для нас город
+        /// </summary>
+        private List<List<int>> path = new List<List<int>>();
+
+        /// <summary>
+        /// Вот именно сейчас у меня с этим проблема, нужно подумать, как определять поездки и добавлять в маршрут
+        /// </summary>
+        /// <param name="ind">НАчало отправления, если мы куда то приезжаем, суда передается новое место</param>
+        /// <param name="end">Самый конечный пункт назначения</param>
+        /// <param name="_path">Путь маршрута, который передается из раза в раз, но из-за особенностей C# надо придумывать обходное решение этой проблемы</param>
+        private void get_path(int ind, int end, List<int> _path)
+        {
+            var d = _data.Rows[ind];
+            var r = new List<int>();
+            r.AddRange(_path);
+            r.Add(ind + 1);
+
+            for (int i = 1; i < _data.Columns.Count; i++)
+            {
+                if (d[i] != DBNull.Value)
+                {
+                    if (i == end)
+                    {
+
+                        path.Add(r);
+                        return;
+                    }
+                    else
+                    {
+                        _path.Add(i);
+                            get_path(i - 1, end, _path);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Этот метод нужен для загрузки доступных Пунктов прибытия, куда можно приехать, если выбран Прямой тип поездки( чтобы сменить тип поездки нужно type_road = 0 )
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void load_end(object sender, EventArgs e)
+        {
+            load_end();
+        }
+    }
+}
